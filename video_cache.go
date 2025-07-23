76dabb446f9e44d90c20703d6e5d2f929424cdb0
Jason Sando <jason.sando@aristocrat.com>
2025-07-23 15:24:20 -0700

    Use hosted sample videos

--- video_cache.go
+++ video_cache.go
@@ -23,18 +23,63 @@ type VideoInfo struct {
 
 var testVideos = []VideoInfo{
 	{
-		Name:       "video-640x360-h264",
-		URL:        "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4",
-		Resolution: "640x360",
+		Name:       "video-240p30-h264",
+		URL:        "https://github.com/jsando/videos-for-testing/releases/download/v1.0/3dtunnel_240p30_h264.mp4",
+		Resolution: "426x240",
 	},
 	{
-		Name:       "video-1080p-h264",
-		URL:        "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_10MB.mp4",
+		Name:       "video-240p60-h264",
+		URL:        "https://github.com/jsando/videos-for-testing/releases/download/v1.0/3dtunnel_240p60_h264.mp4",
+		Resolution: "426x240",
+	},
+	{
+		Name:       "video-720p30-h264",
+		URL:        "https://github.com/jsando/videos-for-testing/releases/download/v1.0/3dtunnel_720p30_h264.mp4",
+		Resolution: "1280x720",
+	},
+	{
+		Name:       "video-720p60-h264",
+		URL:        "https://github.com/jsando/videos-for-testing/releases/download/v1.0/3dtunnel_720p60_h264.mp4",
+		Resolution: "1280x720",
+	},
+	{
+		Name:       "video-1080p30-h264",
+		URL:        "https://github.com/jsando/videos-for-testing/releases/download/v1.0/3dtunnel_1080p30_h264.mp4",
 		Resolution: "1920x1080",
 	},
 	{
-		Name:       "video-4k-h264",
-		URL:        "https://sample-videos.com/video321/mp4/720/big_buck_bunny_720p_10mb.mp4",
+		Name:       "video-1080p60-h264",
+		URL:        "https://github.com/jsando/videos-for-testing/releases/download/v1.0/3dtunnel_1080p60_h264.mp4",
+		Resolution: "1920x1080",
+	},
+	{
+		Name:       "video-2160p30-h264",
+		URL:        "https://github.com/jsando/videos-for-testing/releases/download/v1.0/3dtunnel_2160p30_h264.mp4",
+		Resolution: "3840x2160",
+	},
+	{
+		Name:       "video-2160p60-h264",
+		URL:        "https://github.com/jsando/videos-for-testing/releases/download/v1.0/3dtunnel_2160p60_h264.mp4",
+		Resolution: "3840x2160",
+	},
+	{
+		Name:       "video-hawaii-240p24-h264",
+		URL:        "https://github.com/jsando/videos-for-testing/releases/download/v1.0/hawaii-ocean-sunrise-240p24_h264.mp4",
+		Resolution: "426x240",
+	},
+	{
+		Name:       "video-hawaii-720p24-h264",
+		URL:        "https://github.com/jsando/videos-for-testing/releases/download/v1.0/hawaii-ocean-sunrise-720p24_h264.mp4",
+		Resolution: "1280x720",
+	},
+	{
+		Name:       "video-hawaii-1080p24-h264",
+		URL:        "https://github.com/jsando/videos-for-testing/releases/download/v1.0/hawaii-ocean-sunrise-1080p24_h264.mp4",
+		Resolution: "1920x1080",
+	},
+	{
+		Name:       "video-hawaii-2160p24-h264",
+		URL:        "https://github.com/jsando/videos-for-testing/releases/download/v1.0/hawaii-ocean-sunrise-2160p24_h264.mp4",
 		Resolution: "3840x2160",
 	},
 }
@@ -44,12 +89,12 @@ func NewVideoCache() (*VideoCache, error) {
 	if err != nil {
 		return nil, err
 	}
-	
+
 	cacheDir := filepath.Join(homeDir, ".chromebench", "videos")
 	if err := os.MkdirAll(cacheDir, 0755); err != nil {
 		return nil, err
 	}
-	
+
 	return &VideoCache{cacheDir: cacheDir}, nil
 }
 
@@ -68,15 +113,15 @@ func (vc *VideoCache) IsVideoCached(videoInfo VideoInfo) bool {
 
 func (vc *VideoCache) DownloadVideo(videoInfo VideoInfo) error {
 	localPath := vc.GetVideoPath(videoInfo)
-	
+
 	// Check if already exists
 	if vc.IsVideoCached(videoInfo) {
 		fmt.Printf("Video %s already cached at %s\n", videoInfo.Name, localPath)
 		return nil
 	}
-	
+
 	fmt.Printf("Downloading %s from %s...\n", videoInfo.Name, videoInfo.URL)
-	
+
 	// Create temporary file
 	tmpPath := localPath + ".tmp"
 	out, err := os.Create(tmpPath)
@@ -84,7 +129,7 @@ func (vc *VideoCache) DownloadVideo(videoInfo VideoInfo) error {
 		return err
 	}
 	defer out.Close()
-	
+
 	// Download the file
 	resp, err := http.Get(videoInfo.URL)
 	if err != nil {
@@ -92,51 +137,51 @@ func (vc *VideoCache) DownloadVideo(videoInfo VideoInfo) error {
 		return err
 	}
 	defer resp.Body.Close()
-	
+
 	if resp.StatusCode != http.StatusOK {
 		os.Remove(tmpPath)
 		return fmt.Errorf("bad status: %s", resp.Status)
 	}
-	
+
 	// Get the size
 	size := resp.ContentLength
-	
+
 	// Create progress reader
 	pr := &progressReader{
 		Reader: resp.Body,
 		Total:  size,
 		Name:   videoInfo.Name,
 	}
-	
+
 	// Copy the file
 	_, err = io.Copy(out, pr)
 	if err != nil {
 		os.Remove(tmpPath)
 		return err
 	}
-	
+
 	// Close the file
 	out.Close()
-	
+
 	// Rename to final path
 	if err := os.Rename(tmpPath, localPath); err != nil {
 		os.Remove(tmpPath)
 		return err
 	}
-	
+
 	fmt.Printf("\nDownloaded %s successfully\n", videoInfo.Name)
 	return nil
 }
 
 func (vc *VideoCache) EnsureAllVideos() error {
 	fmt.Println("Checking video cache...")
-	
+
 	for _, video := range testVideos {
 		if err := vc.DownloadVideo(video); err != nil {
 			return fmt.Errorf("failed to download %s: %v", video.Name, err)
 		}
 	}
-	
+
 	fmt.Println("All videos cached successfully")
 	return nil
 }
@@ -152,19 +197,19 @@ type progressReader struct {
 func (pr *progressReader) Read(p []byte) (int, error) {
 	n, err := pr.Reader.Read(p)
 	pr.Downloaded += int64(n)
-	
+
 	// Print progress every 1MB
 	if pr.Downloaded-pr.lastPrint > 1024*1024 || pr.Downloaded == pr.Total || err == io.EOF {
 		pr.lastPrint = pr.Downloaded
 		if pr.Total > 0 {
 			percent := float64(pr.Downloaded) / float64(pr.Total) * 100
-			fmt.Printf("\r%s: %.1f%% (%.1f MB / %.1f MB)", 
+			fmt.Printf("\r%s: %.1f%% (%.1f MB / %.1f MB)",
 				pr.Name,
 				percent,
 				float64(pr.Downloaded)/(1024*1024),
 				float64(pr.Total)/(1024*1024))
 		}
 	}
-	
+
 	return n, err
 }
